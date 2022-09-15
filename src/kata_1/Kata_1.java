64a52d611d2996d76796e5bf8c9dee9a5e13a91d
Elena Navarro <99991881+ElenaaNavarroo@users.noreply.github.com>
2022-09-15 16:55:44 +0100

    Add files via upload
    Version 2 de la kata 1

--- src/kata_1/Kata_1.java
+++ src/kata_1/Kata_1.java
@@ -1,12 +1,20 @@
 package kata_1;
 
 import java.util.Date;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class Kata_1 {
 
     public static void main(String[] args) {
-        Person person = new Person ("Marcos", new Date(102, 8, 24));
-        System.out.println(person.getName() + " tiene " + person.getAge());
+        Calendar date;
+        date = GregorianCalendar.getInstance();
+        date.set(2002, 8, 25);
+        
+        
+        Person person = new Person ("Marcos", date);
+        System.out.println(person.getName() + " tiene " + person.getAge() + " años ");
+        
     }
     
 }
